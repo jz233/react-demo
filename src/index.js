a6da365e8d6d4fb320eabe5a20a7734483ca59b5
jason.zhang <douj27@gmail.com>
2018-01-22 21:50:39 +0800

    Lists and Keys finished

--- src/index.js
+++ src/index.js
@@ -379,11 +379,91 @@ class Page extends React.Component {
   }
 }
 
+// ReactDOM.render(
+//   <Page />,
+//   document.getElementById('root')
+// );
+
+/*
+  ################################################
+      Lists and Keys
+  ################################################
+*/
+// const numbers = [1, 2, 3, 4, 5];
+// const listItems = numbers.map((number) =>
+//   <li>{number}</li>
+// );
+//
+// ReactDOM.render(
+//   <ul>{listItems}</ul>,
+//   document.getElementById('root')
+// );
+function ListItem(props) {
+  return(
+    <li>{props.value}</li>
+  );
+}
+function NumberList(props) {
+  const numbers = props.numbers;
+  // const listItems = numbers.map((number)=>
+  //   <ListItem key={number.toString()}   //key一定要放在顶层标签上，不能放在下层ListItem方法中的<li>上面
+  //     value={number}/>
+  // );
+  //
+  // return (
+  //   <ul>{listItems}</ul>
+  // );
+  return(
+    <ul>
+      {numbers.map((number) =>
+        <ListItem key={number.toString()}
+          value={number} />
+      )}
+    </ul>
+  );
+}
+
+const numbers = [1, 2, 3, 4, 5];
 ReactDOM.render(
-  <Page />,
+  <NumberList numbers={numbers} />,
   document.getElementById('root')
 );
 
+function Blog(props){
+  const sidebar = (
+    <ul>
+      {props.posts.map((post) =>
+        <li key={post.id}>
+          {post.title}
+        </li>
+      )}
+    </ul>
+  );
+
+  const content = props.posts.map((post) =>
+    <div key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content}</p>
+    </div>
+  );
+
+  return (
+    <div>
+      {sidebar}
+      <hr />
+      {content}
+    </div>
+  );
+}
+
+const posts = [
+  {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
+  {id: 2, title: 'Installation', content: 'You can install React from npm.'}
+];
+// ReactDOM.render(
+//   <Blog posts={posts}/>,
+//   document.getElementById('root')
+// );
 
 
 
