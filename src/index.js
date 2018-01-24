6c6a34cc5c32b11a79ea5d5bb47adadb17a18f67
jason.zhang <douj27@gmail.com>
2018-01-24 16:21:53 +0800

    Composition vs Inheritance finished

--- src/index.js
+++ src/index.js
@@ -777,16 +777,142 @@ function tryConvert(temperature, convert){
   return rounded.toString();
 }
 
+// ReactDOM.render(
+//   <Calculator />,
+//   document.getElementById('root')
+// );
 
 
+/*
+  ################################################
+      Composition vs Inheritance
+  ################################################
+*/
+function FancyBorder(props) {
+  return(
+    <div className={'FancyBorder FancyBorder-'+ props.color}>
+      {props.children}
+    </div>
+  );
+}
+
+// function WelcomeDialog(props) {
+//   return (
+//     <FancyBorder color='blue'>
+//       <h1 className="Dialog-title">
+//         Welcome
+//       </h1>
+//       <p className="Dialog-message">
+//         Thank you for visiting our spacecraft!
+//       </p>
+//     </FancyBorder>
+//   );
+// }
+
+// ReactDOM.render(
+//   <WelcomeDialog />,
+//   document.getElementById('root')
+// );
+
+function Contacts() {
+  return <div className="Contacts" />;
+}
+
+function Chat() {
+  return <div className="Chat" />;
+}
+
+function SplitPane(props) {
+  return (
+    <div className="SplitPane">
+      <div className="SplitPane-left">
+        {props.left}
+      </div>
+      <div className="SplitPane-right">
+        {props.right}
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <SplitPane
+      left={
+        <Contacts />
+      }
+      right={
+        <Chat />
+      } />
+  );
+}
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
+
+function Dialog(props) {
+  return (
+    <FancyBorder color="blue">
+      <h1 className="Dialog-title">
+        {props.title}
+      </h1>
+      <p className="Dialog-message">
+        {props.message}
+      </p>
+      {props.children}
+    </FancyBorder>
+  );
+}
+
+// function WelcomeDialog(props){
+//   return (
+//     <Dialog
+//       title="Welcome"
+//       message="Thank you for visiting our spacecraft!"
+//     />
+//   );
+// }
+
+class SignUpDialog extends React.Component {
+  constructor(props){
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
+    this.state = {
+      login: '',
+    }
+  }
+
+  handleChange(ev){
+    this.setState({
+      login: ev.target.value
+    });
+  }
+
+  handleSignUp(){
+    alert(`Welcome aboard, ${this.state.login}`);
+  }
+
+  render(){
+    return (
+      <Dialog title="Mars Exploration Program"
+              message="How should we refer to you?">
+          <input value={this.state.login}
+                 onChange={this.handleChange}/>
+          <button onClick={this.handleSignUp}>
+            Sign Up
+          </button>
+      </Dialog>
+    );
+  }
+}
 
 ReactDOM.render(
-  <Calculator />,
+  <SignUpDialog />,
   document.getElementById('root')
 );
-
-
-
 
 
 
